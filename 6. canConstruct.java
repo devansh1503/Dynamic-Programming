359aa4afdede702c7bae0251fd3f92f2d2978029
Devansh Abrol <80891722+devansh1503@users.noreply.github.com>
2022-06-02 10:36:23 +0530

    Update 6. canConstruct.java

--- 6. canConstruct.java
+++ 6. canConstruct.java
@@ -20,3 +20,30 @@ public class canConst {
         System.out.println(canconst("abcdef",arr));
     }
 }
+
+Tabulation:-
+import java.util.*;
+public class canConst_tab {
+    public static boolean canConst(String s, String[] arr) {
+        boolean[] tab = new boolean[s.length() + 1];
+        Arrays.fill(tab,false);
+        tab[0] = true;
+        for (int i = 0; i < tab.length; i++) {
+            if(tab[i]==true) {
+                for (String a : arr) {
+                    int n = a.length();
+                    if(i+n>s.length()) continue;
+                    if (s.substring(i, i + n).equals(a)) {
+                        tab[i + n] = true;
+                    }
+                }
+            }
+
+        }
+        return tab[s.length()];
+    }
+    public static void main(String[]args){
+        String[]arr = {"ab","abc","cd","def","abcd"};
+        System.out.println(canConst("abcdef",arr));
+    }
+}
